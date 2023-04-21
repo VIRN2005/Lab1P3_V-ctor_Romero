7cfbe7cd54cd33654cab73c7d40c374c45c1a39f
Víctor Romero <virn2005@gmail.com>
2023-04-21 14:43:35 -0600

    Ejercicio 2 - Listo

--- Lab1P3_Víctor_Romero.cpp
+++ Lab1P3_Víctor_Romero.cpp
@@ -17,7 +17,7 @@ int main() {
 		cout << ">> Ingrese una opcion del menu: ";
 		cin >> opcion;
 
-		cout << "La opcion escojida es :" << opcion;
+		cout << "La opcion escojida es :" << opcion << endl << endl;
 
 		switch (opcion) {
 		case 0: {
@@ -27,13 +27,37 @@ int main() {
 			  break;
 
 		case 1: {
-			cout << " - 1) ERROR NUMERICO - \n";
+			cout << "- 1) ERROR NUMERICO - \n";
 
 		}
 			  break;
 
 		case 2: {
-			cout << " - 2) DIVISION DE 2 ENTRADAS - \n";
+			cout << "- 2) DIVISION DE 2 ENTRADAS - \n";
+
+			int P;
+			cout << "> Ingrese el valor de P: ";
+			cin >> P;
+
+			int A;
+			cout << "> Ingrese el valor de A: ";
+			cin >> A;
+
+			int B;
+			cout << "> Ingrese el valor de B: ";
+			cin >> B;
+
+			int sum=0;
+
+				for (int i = 0; i < P; i++) {
+					if (B > 0) {
+						sum += A / B;
+						B--;
+					}
+				}
+			
+			cout << "M = " << sum << endl << endl;
+
 		}
 			  break;
 
