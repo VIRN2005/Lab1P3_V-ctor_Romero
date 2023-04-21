2070201a2c8b80eca1d575c329d77d89e6251073
Víctor Romero <virn2005@gmail.com>
2023-04-21 15:38:02 -0600

    Commit ejercicio 1

--- Lab1P3_Víctor_Romero.cpp
+++ Lab1P3_Víctor_Romero.cpp
@@ -21,13 +21,55 @@ int main() {
 
 		switch (opcion) {
 		case 0: {
-			cout << "Gracias por participar!\n";
-
+			cout << "Gracias por participar!\n"
+				<< "Feliz dia :D";
 		}
 			  break;
 
 		case 1: {
 			cout << "- 1) ERROR NUMERICO - \n";
+
+			double N;
+			cout << "Ingrese el valor de N :";
+			cin >> N;
+
+			float XF = 1 / (float)N;
+			double XD = 1 / (double)N;
+			long double XL = 1 / (long double)N;
+
+			float SF = 0;
+			double SD = 0;
+			long double SL = 0;
+
+			for (size_t i = 0; i < N; i++){
+				SF += XF;
+				SD += XD;
+				SL += XL;
+			}
+			
+			float EF = 1.0 - SF;
+			double ED = 1.0 - SD;
+			long double EL = 1.0 - SL;
+
+			if (EF < 0) {
+				EF *= -1;
+			}
+			else if (ED < 0) {
+				ED *= -1;
+			}
+			else if (EL < 0) {
+				EL *= -1;
+			}
+
+			if (EF > 0 && ED > 0 && EL > 0) {
+				EF = 1.0 - SF;
+				ED = 1.0 - SD;
+				EL = 1.0 - SD;
+			}
+
+			cout << "Valor usando DOUBLE: " << ED << endl;
+			cout << "Valor usando FLOAT: " << EF << endl;
+			cout << "Valor usando LONG DOUBLE: " << EL << endl;
 
 		}
 			  break;
